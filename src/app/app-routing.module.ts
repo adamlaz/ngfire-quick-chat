f0f5e9e101a2ebc933d8ae009319bf025e7360ba
Adam Lazarus <adam@ventract.com>
2017-07-23 13:46:31 -0400

    chore: adding basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './+main/main.component';
+import { SigninComponent } from './+signin/signin.component';
+import { MypostsComponent } from './+myposts/myposts.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+
+{ path: '', component: MainComponent },
+{ path: 'signin', component: SigninComponent },
+{ path: 'myposts', component: MypostsComponent },
+{ path: '**', redirectTo: '' },
+
 ];
 
 @NgModule({
